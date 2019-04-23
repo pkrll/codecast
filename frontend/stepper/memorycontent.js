95feae6ac734c150f0edfb4de08543ed262112e4
Ardalan Samimi <ardalan.samimi@gmail.com>
2019-04-23 18:28:03 +0200

    Added MemoryContent type and changed how the memory content is collected

---
+++ frontend/stepper/memorycontent.js
@@ -0,0 +1,88 @@
+import * as C from 'persistent-c';
+import { getNumber, readValue } from './views/utils';
+
+export const Types = {
+  BUILTIN: "builtin",
+  POINTER: "pointer",
+  SCALAR: "scalar",
+  RECORD: "record"
+};
+
+function getType(type) {
+  switch (type.kind) {
+    case Types.POINTER:
+      return {kind: type.kind, type: getType(type.pointee)};
+      break;
+    case Types.BUILTIN:
+      return {kind: Types.SCALAR, name: type.repr};
+      break;
+    case Types.RECORD:
+      return {kind: type.kind, name: type.name};
+      break;
+    case Types.SCALAR:
+      return {kind: type.current.type.kind, name: type.current.type.repr};
+      break;
+    default:
+      return {kind: "unknown", name: "unknown"};
+  }
+}
+
+/**
+ * Generates a new MemoryContent type.
+ *
+ * @param       {Object} context [description]
+ * @param       {Object} ref     [description]
+ * @param       {Object} start   [description]
+ * @constructor
+ */
+export function MemoryContent(context, ref, {start, end, free}) {
+  if (ref.type.kind !== Types.POINTER) return undefined;
+
+  const {core}  = context;
+  const pointer = ref.type.pointee;
+
+  const value = (pointer.repr == "void")
+              ? {}
+              : readValue(context, ref.type, ref.address);
+
+  // The type of the allocated block consist of kind and name
+  // where kind is pointer|scalar|record and name is either the
+  // name of a record/structure or the name of the built-in type.
+  this.type = getType(pointer);
+  this.size = pointer.size;
+  this.address = ref.address;
+  this.fields  = [];
+  this.free = free;
+  this.end = end;
+  this.fieldAddresses = {};
+
+  if (this.type.kind == Types.SCALAR) {
+    // Scalar types does not have fields built in
+    const numberOfFields = (end - start + 1) / this.size;
+    for (let i = 0; i < numberOfFields; i++) {
+      const name    = i.toString();
+      const size    = this.size;
+      const type    = this.type;
+      const address = this.address + (i * this.size);
+
+      this.fields.push(buildField(i.toString(), this.size, this.type, address));
+      this.fieldAddresses[address] = this.address;
+    }
+    // Update the size to reflect the actual size of the memory block
+    this.size = numberOfFields * this.size;
+  } else {
+    for (let index in value.fields) {
+      const field = value.fields[index];
+      const name  = field.name;
+      const size  = field.content.current.type.size;
+      const type  = getType(field.content.current.type);
+
+      this.fields.push(buildField(name, size, type, field.address));
+      this.fieldAddresses[field.address] = this.address;
+    }
+  }
+}
+
+function buildField(name, size, type, address) {
+  return {name, size, type, address};
+}
--- frontend/stepper/views/heapgraph.js
+++ frontend/stepper/views/heapgraph.js
@@ -3,14 +3,10 @@ import classnames from 'classnames';
 import * as C from 'persistent-c';
 import { getNumber, readValue } from './utils';
 import { enumerateHeapBlocks } from '../heap';
+import { MemoryContent, Types } from '../memorycontent';
 import Immutable from 'immutable';
 
-const Types = {
-  BUILTIN: "builtin",
-  POINTER: "pointer",
-  SCALAR: "scalar",
-  RECORD: "record"
-};
+import './../../style.scss';
 
 const Dimensions = {
   HEIGHT: 20,
@@ -33,9 +29,7 @@ class HeapGraph extends React.PureComponent {
       <div style={{background: `rgb(240, 240, 240)`, width: `100%`, height: height}}>
         <svg width="100%" height="100%" aria-labelledby="title desc">
           <defs>
-            <marker id="arrow" viewBox="0 0 10 10" refX="5" refY="5" markerWidth="5" markerHeight="5" fill="black" orient="auto-start-reverse">
-            <path d="M 0 0 L 10 5 L 0 10 z" />
-            </marker>
+            <marker id="arrow" viewBox="0 0 10 10" refX="5" refY="5" markerWidth="5" markerHeight="5" fill="black" orient="auto-start-reverse"><path d="M 0 0 L 10 5 L 0 10 z" /></marker>
           </defs>
           {
             Object.keys(memory.blocks).map((key, index) => {
@@ -66,21 +60,25 @@ class HeapGraph extends React.PureComponent {
   }
 }
 
-function Line({fromAddress, toAddress, startAddress}) {
-  const startY = (fromAddress - startAddress) * Dimensions.HEIGHT + 5;
-  const finalY = (toAddress - startAddress) * Dimensions.HEIGHT;
+class Line extends React.PureComponent {
+  render() {
+    const {fromAddress, toAddress, startAddress} = this.props;
 
-  const startX = Dimensions.X + Dimensions.WIDTH;
-  const finalX = Dimensions.X + Dimensions.WIDTH + 100;
+    const startY = (fromAddress - startAddress) * Dimensions.HEIGHT + 5;
+    const finalY = (toAddress - startAddress) * Dimensions.HEIGHT;
 
-  const path = " M " + startX + "," + startY
-             + " C " + finalX + "," + startY
-             + "   " + finalX + "," + finalY
-             + "   " + startX + "," + finalY;
+    const startX = Dimensions.X + Dimensions.WIDTH;
+    const finalX = Dimensions.X + Dimensions.WIDTH + 100;
 
-  return (
-      <path fill="transparent" stroke="black" d={path} markerEnd="url(#arrow)" />
-  )
+    const d = " M " + startX + "," + startY
+            + " C " + finalX + "," + startY
+            + "   " + finalX + "," + finalY
+            + "   " + startX + "," + finalY;
+
+    return (
+        <path className="pointerArrow" pointerEvents="visiblePoint" d={d} markerEnd="url(#arrow)"/>
+    );
+  };
 }
 
 function Variable({memory, variable}) {
@@ -101,12 +99,12 @@ function Variable({memory, variable}) {
   return (
     <g fill="white">
       <text y={previousSize + 10} x={Dimensions.X} fontSize='12px' fontWeight='bold' fill='grey'>
-        {variable.type.type.name}
+        {variable.type.name}
       </text>
       {content}
     </g>
   );
-  // <line x1={arrow_x1} y1="30" x2={arrow_x2} y2="30" stroke="black" markerEnd="url(#arrow)"/>
+
 }
 
 function Field({memory, field, offsetTop}) {
@@ -128,14 +126,6 @@ function Field({memory, field, offsetTop}) {
   );
 }
 
-function getSizeOf(pointer) {
-  if (pointer.fields) {
-    return pointer.fields.length;
-  }
-
-  return 1;
-}
-
 function getValueOf(data) {
   if (data !== undefined) {
     const type = data.constructor.name;
@@ -147,169 +137,65 @@ function getValueOf(data) {
 }
 
 function mapMemory(context, memory, startAddress, maxAddress) {
-  const { core, oldCore } = context;
   let { scope } = context.core;
-
-  let variables = {};
-  let pointers  = {};
-  let fields = {};
 
   while (scope && scope.limit <= maxAddress + 1) {
     const {kind, name, ref} = scope;
     if (kind == "variable") {
-      const {block, addr} = unpack(context, name, ref, startAddress, maxAddress);
-      if (block.type.kind == Types.POINTER) {
-        pointers[block.address] = block;
-        fields[block.address] = addr;
-      } else {
-        variables[ref.address] = block;
-      }
+      // TODO: Keep track of stack variables?
     }
 
     scope = scope.parent;
   }
 
-  let lastAddress = core.heapStart;
+  let lastAddress = context.core.heapStart;
+  let blocks = {};
 
-  for (let block of enumerateHeapBlocks(core)) {
-    if (pointers.hasOwnProperty(block.start)) {
-      let content = pointers[block.start];
+  for (let block of enumerateHeapBlocks(context.core)) {
+    blocks[block.start] = {
+      start: block.start,
+      end: block.end,
+      free: block.free
+    };
 
-      content.free = block.free;
-      if (content.endAdress == undefined) {
-        content.endAdress = block.end;
-
-        const sizeOfType = content.size;
-        const blockSize  = (block.end - block.start + 1);
-        // Arrays
-        if (sizeOfType <= blockSize && content.fields.length < 1) {
-          addFieldsToBlock(content, sizeOfType, blockSize, memory);
-        }
+    if (block.free) {
+      // Makes sure a free'd block is marked as free
+      if (memory.blocks.hasOwnProperty(block.start)) {
+        memory.blocks[block.start].free = block.free;
       }
-      memory.blocks[block.start] = content;
-      Object.assign(memory.fields, fields[block.start]);
-    } else if (block.free) { // The first available memory block
+
       lastAddress = block.ref.address;
     }
   }
 
-  lastAddress -= core.heapStart;
-  pointers = {};
+  lastAddress -= context.core.heapStart;
 
-  for (let entry of core.memoryLog) {
+  for (let entry of context.core.memoryLog) {
     const op = entry[0];
     if (op == "store") {
       const source = entry[1];
-      const target = entry[2];
+      const value  = entry[2];
+
+      if (blocks.hasOwnProperty(value.address)) {
+        const content = new MemoryContent(context, value, blocks[value.address]);
+
+        memory.blocks[content.address] = content;
+        Object.assign(memory.fields, content.fieldAddresses);
+      }
+
       if (memory.fields.hasOwnProperty(source.address)) {
-        const type = target.constructor.name;
+        const type = value.constructor.name;
 
         if (type == "IntegralValue") {
-          memory.values[source.address] = new ValueType(source.address, target.number);
+          memory.values[source.address] = new ValueType(source.address, value.number);
         } else {
-          memory.values[source.address] = new PointerType(source.address, target.address);
+          memory.values[source.address] = new PointerType(source.address, value.address);
         }
       }
     }
   }
 
   return {memory, lastAddress};
-}
-
-function unpack(context, name, ref, startAddress, endAddress) {
-  const { core } = context;
-  const type = ref.type.pointee.kind;
-  let refType;
-
-  switch (type) {
-    case Types.BUILTIN:
-      refType = ref.type;
-    break;
-    case Types.POINTER:
-      refType = ref.type.pointee;
-    break;
-    default:
-      console.log("Unrecognized type: " + ref.type.pointee.kind);
-    return;
-  }
-
-  const value = (refType.pointee.repr == "void")
-              ? {}
-              : readValue(context, refType, ref.address);
-
-  let block = {
-    name: name,
-    type: getType(ref.type.pointee),
-    size: refType.pointee.size,
-    fields: []
-  };
-
-  if (block.type.kind == Types.POINTER) {
-    block.address = C.readValue(core, ref).toInteger();
-    block.free = false;
-  } else {
-    block.content = C.readValue(core, ref).toInteger();
-  }
-
-  let addr = {};
-  addr[block.address] = block.address;
-
-  for (let index in value.fields) {
-    const field   = value.fields[index];
-    const address = block.address + refType.pointee.fieldMap[field.name].offset;
-
-    block.fields.push({
-      name: field.name,
-      size: field.content.current.type.size,
-      type: getType(field.content.current.type),
-      address: address
-    });
-
-    addr[address] = block.address;
-  }
-
-  return { block, addr };
-}
-
-function getType(type) {
-  switch (type.kind) {
-    case Types.POINTER:
-      return {kind: type.kind, type: getType(type.pointee)};
-      break;
-    case Types.BUILTIN:
-      return {kind: Types.SCALAR, name: type.repr};
-      break;
-    case Types.RECORD:
-      return {kind: type.kind, name: type.name};
-      break;
-    case Types.SCALAR:
-      return {kind: type.current.type.kind, name: type.current.type.repr};
-      break;
-    default:
-      return {kind: "unknown", name: "unknown"};
-  }
-}
-
-function addFieldsToBlock(content, sizeOfType, blockSize, memory) {
-  const numberOfSlots = blockSize / sizeOfType;
-  let fields = {};
-
-  for (let i = 0; i < numberOfSlots; i++) {
-    const address = content.address + (i * sizeOfType);
-    content.fields.push({
-      name: i.toString(),
-      size: sizeOfType,
-      type: content.type,
-      address: address
-    });
-
-    fields[address] = content.address;
-    // if (memory.values[address] == undefined) {
-    //   memory.values[address] = new ValueType(address, randomString(7));
-    // }
-  }
-
-  Object.assign(memory.fields, fields);
 }
 
 function ValueType(source, value) {
